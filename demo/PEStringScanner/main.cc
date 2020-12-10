eff779819c345627749bb3c89aca5ffe812a15b3
ty969975553 <969975553@qq.com>
2020-12-10 20:06:38 +0800

    bug fix

--- demo/PEStringScanner/main.cc
+++ demo/PEStringScanner/main.cc
@@ -6,51 +6,20 @@
 #include <vector>
 
 #include "base/command_line.h"
+#include "base/encode/hex.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/test/unittest.h"
-
-using namespace base;
-
-using uchar = unsigned char;
+#include "ui/console/console.h"
 
 namespace
 {
     std::vector<std::wstring> scan_strings;
-    std::vector<int> encode_vector = {CP_UTF8, 936};
-    enum UnicodeMode
-    {
-        BE, LE
-    };
+    std::vector<int> encode_vector = {CP_UTF8, 936, -1};  //-1 unicode
     std::vector<std::wstring> result;
     bool case_insensitive = false;
 
-    void ConvertCharacterEncodingToBytes(const std::wstring str, int code,
-                                         std::vector<uchar>& data)
-    {
-        if (!data.empty())
-        {
-            data.clear();
-        }
-        int nSize = WideCharToMultiByte(code, 0, str.c_str(), -1, 0, 0, 0, 0);
-        uchar* pBuff = new uchar[nSize]{0};
-        ::WideCharToMultiByte(code, 0, str.c_str(), -1, (LPSTR)pBuff, nSize, 0,
-                              0);
-        data.insert(data.end(), pBuff, pBuff + nSize);
-        delete[] pBuff;
-        return;
-    }
-
-    void ConvertUnicodeEncodingToBytes(const std::wstring str, int code,
-                                       std::vector<uchar>& data)
-    {
-        if (!data.empty())
-        {
-            data.clear();
-        }
-    }
-
-    bool CompareChar(char c1, char c2)
+    bool CompareChar(unsigned char c1, unsigned char c2)
     {
         if (!case_insensitive)
         {
@@ -59,134 +28,87 @@ namespace
         return c1 == c2;
     }
 
-    void FindString(const std::vector<char> pe_buffer)
+    void FindString(const std::vector<uint8_t> pe_buffer)
     {
-        bool find = false;
-        std::vector<uchar> temp;
+        std::vector<uint8_t> temp;
         for (auto item : scan_strings)
         {
+            bool find = false;
             for (auto encode : encode_vector)
             {
-                ConvertCharacterEncodingToBytes(item, encode, temp);
+                base::encoding::ConvertCharacterEncodingToBytes(item, encode,
+                                                                temp);
                 auto it = std::search(pe_buffer.begin(), pe_buffer.end(),
-                                      temp.begin(), temp.end(), CompareChar);
-                if (it != std::end(pe_buffer))
+                                      temp.begin(), temp.end());
+                if (it != pe_buffer.end())
                 {
                     find = true;
                     break;
                 }
             }
-
             if (find) result.push_back(item);
         }
     }
-}  // namespace
 
-namespace test
-{
-    TEST_FILE
+    void GetPEBuffer(std::wstring path, std::vector<uint8_t>& pe_buffer) 
     {
-        TEST_CATEGORY(L"function_test")
+        HANDLE hFile =
+            CreateFile(path.c_str(), GENERIC_READ, FILE_SHARE_READ,
+                       NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+        if (INVALID_HANDLE_VALUE == hFile)
         {
-            TEST_CASE(L"Convert")
-            {
-                std::vector<uchar> data;
-                // 4142 43bf b4cd bc
-                ConvertCharacterEncodingToBytes(L"ABC看图", 936, data);
-                TEST_ASSERT(data[0] == 0x41);
-                TEST_ASSERT(data[1] == 0x42);
-                TEST_ASSERT(data[2] == 0x43);
-                TEST_ASSERT(data[3] == 0xbf);
-                TEST_ASSERT(data[4] == 0xb4);
-                TEST_ASSERT(data[5] == 0xcd);
-                TEST_ASSERT(data[6] == 0xbc);
+            return;
+        }
 
-                // 41 42	43 e79c 8be5 9bbe
-                ConvertCharacterEncodingToBytes(L"ABC看图", CP_UTF8, data);
-                std::vector expected = {0x41, 0x42, 0x43, 0xe7, 0x9c,
-                                        0x8b, 0xe5, 0x9b, 0xbe, 0x00};
-                TEST_ASSERT(std::equal(data.begin(), data.end(),
-                                       expected.begin(), expected.end()));
-            });
+        int nLen = GetFileSize(hFile, NULL);
 
-            TEST_CASE(L"FindString")
-            {
+        if (0 == nLen)
+        {
+            return;
+        }
 
-            });
-        });
+        HANDLE hMap =
+            CreateFileMapping(hFile, NULL, PAGE_READONLY, 0, nLen, NULL);
+
+        UCHAR* pMap = (UCHAR*)MapViewOfFile(hMap, FILE_MAP_READ, 0, 0, nLen);
+        if (NULL == pMap)
+        {
+            int n = GetLastError();
+            return;
+        }
+
+        pe_buffer.assign(pMap, pMap + nLen);
+
+        if (pMap)
+        {
+            UnmapViewOfFile(pMap);
+        }
+        if (hMap)
+        {
+            CloseHandle(hMap);
+        }
+        CloseHandle(hFile);
     }
-}  // namespace test
+}  // namespace
 
 int wmain(int argc, wchar_t* argv[])
 {
-#define UNITTEST
-#if defined UNITTEST
-    Test::UnitTest::RunAndDisposeTests(argc, argv);
-#endif
-    // scan_strings = {L"ABC看图"};
+    base::CommandLine cmdline(argc, argv);
+    base::UsageVector usevec = {
+        {L"exe", L"need scanner exe filepath", false, true, nullptr},
+        {L"json", L"strings vector json format", false, false, nullptr},
+    };
+    scan_strings = {L"睦欣网络",L"KPZIP",L"KUAIZIP",L"SHZHANMENG.COM",L"广乐",L"快压",L"护眼精灵",L"ABC看图",L"JCWallpaper",L"LSZIP",L"CommandLine"};
+    cmdline.SetCommandInfos(usevec);
+    cmdline.PrintParseOptions();
 
-    // 0, 0);
-
-    // std::ifstream t("file.txt");
-    // std::string str(std::istreambuf_iterator<char>(t),
-    //                std::istreambuf_iterator<char>());
-
-    // std::vector<char> pe_buffer;
-    // std::copy(std::istreambuf_iterator<char>(t),
-    //          std::istreambuf_iterator<char>(), back_inserter(pe_buffer));
-
-    // std::vector<unsigned char> buffer;
-    // for (size_t i = 0; i < str1.length(); i++)
-    //{
-    //    buffer.push_back(str1[0] >> 8);
-    //    buffer.push_back(str1[0] & 0x00FF);
-    //}
-    return 0;
-    /* CommandLine cmdline(argc, argv);
-     UsageVector usevec = {
-         {L"exe", L"need scanner exe filepath", false, true, nullptr},
-         {L"json", L"strings vector json format", false, true, nullptr},
-     };
-     cmdline.SetCommandInfos(usevec);
-     cmdline.PrintParseOptions();
-
-     fspath exe_path = fspath(cmdline.get<std::string>(L"exe").value());*/
-    /* HANDLE hFile = CreateFile(exe_path.native().c_str(), GENERIC_READ,
-     FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL); if
-     (INVALID_HANDLE_VALUE == hFile)
-     {
-         return;
-     }
-
-     int nLen = GetFileSize(hFile, NULL);
-
-     if (0 == nLen)
-     {
-         return;
-     }
-
-     HANDLE hMap = CreateFileMapping(hFile, NULL, PAGE_READONLY, 0, nLen, NULL);
-
-     UCHAR* pMap = (UCHAR*)MapViewOfFile(hMap, FILE_MAP_READ, 0, 0, nLen);
-     if (NULL == pMap)
-     {
-         int n = GetLastError();
-         return;
-     }
-
-
-
-     if (pMap)
-     {
-         UnmapViewOfFile(pMap);
-     }
-     if (hMap)
-     {
-         CloseHandle(hMap);
-     }
-     CloseHandle(hFile);*/
-
-    // std::vector<string>
-
+    std::vector<uint8_t> pe_buffer;
+    GetPEBuffer(cmdline.get<std::wstring>(L"exe").value(), pe_buffer);
+    FindString(pe_buffer);
+    for (auto item : result)
+    {
+        ui::console::SetColor(1,0,0,1);
+        ui::console::WriteLine(item);
+    }
     return 0;
 }
