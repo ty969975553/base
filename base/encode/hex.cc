352fce2099bedc19e5e64758c5f59e4a0d62e65e
ty969975553 <969975553@qq.com>
2020-12-10 20:06:55 +0800

    bug fix

--- base/encode/hex.cc
+++ base/encode/hex.cc
@@ -1,10 +1,12 @@
 #include "base/encode/hex.h"
 
+#include <windows.h>
+
 #include <sstream>
 
 namespace base
 {
-    namespace encode
+    namespace encoding
     {
         std::string EncodeHex(const std::string& src_str, bool upper)
         {
@@ -24,17 +26,57 @@ namespace base
 
         std::wstring DecodeHex(const std::vector<uint8_t>& binary)
         {
-            return L"";
+            std::vector<wchar_t> temp;
+            wchar_t c;
+            for (size_t i = 0; i < binary.size(); i++)
+            {
+                if (i % 2)
+                { 
+                    c = c  + (binary[i]<<8);
+                    temp.push_back(c);
+                }
+                else
+                {
+                    c = binary[i] ;                  
+                }
+            }
+            return std::wstring(temp.begin(), temp.end());
         }
 
         void EncodeHex(const std::wstring& input, std::vector<uint8_t>& result)
         {
+            if (!result.empty())
+            {
+                result.clear();
+            }
             for (size_t i = 0; i < input.size(); ++i)
             {
                 wchar_t c = (wchar_t)input[i];
-                result.push_back((c >> 8) & 0xff);
                 result.push_back(c & 0xff);
+                result.push_back((c >> 8) & 0xff);
             }
         }
-    }  // namespace encode
+
+        void ConvertCharacterEncodingToBytes(const std::wstring& input, int code,
+                                             std::vector<uint8_t>& result)
+        {
+            if (!result.empty())
+            {
+                result.clear();
+            }
+
+            if (code == -1)
+            {
+                return EncodeHex(input, result);
+            }
+            int nSize =
+                WideCharToMultiByte(code, 0, input.c_str(), -1, 0, 0, 0, 0);
+            uint8_t* pBuff = new uint8_t[nSize]{0};
+            ::WideCharToMultiByte(code, 0, input.c_str(), -1, (LPSTR)pBuff, nSize,
+                                  0, 0);
+            result.insert(result.end(), pBuff, pBuff + nSize - 1);
+            delete[] pBuff;
+            return;
+        }
+    }  // namespace encoding
 }  // namespace base
--- base/encode/hex.h
+++ base/encode/hex.h
@@ -7,12 +7,15 @@
 
 namespace base
 {
-    namespace encode
+    namespace encoding
     {
         std::string EncodeHex(const std::string& src_str, bool upper);
 
         std::wstring DecodeHex(const std::vector<uint8_t>& binary);
 
         void EncodeHex(const std::wstring& input, std::vector<uint8_t>& result);
-    }  // namespace encode
+
+        void ConvertCharacterEncodingToBytes(const std::wstring& input, int code,
+                                             std::vector<uint8_t>& result);
+    }  // namespace encoding
 }  // namespace base
